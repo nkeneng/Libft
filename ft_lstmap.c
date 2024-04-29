178430abb1b0599ef0869b124a7584c3a8280d4a
steven Nkeneng <snkeneng@student.42berlin.de>
2024-04-29 20:53:59 +0200

    lstmap done

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: snkeneng <snkeneng@student.42berlin.       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 19:27:09 by snkeneng          #+#    #+#             */
-/*   Updated: 2024/04/29 20:15:42 by snkeneng         ###   ########.fr       */
+/*   Updated: 2024/04/29 20:52:38 by snkeneng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,24 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
+	t_list	*start;
 
 	if (lst && f && del)
 	{
-		new_lst = malloc(sizeof(t_list) * ft_lstsize(lst));
+		new_lst = ft_lstnew(f(lst->content));
 		if (new_lst)
 		{
-			while (lst)
+			start = new_lst;
+			while (lst->next)
 			{
-				if (!f(lst->content))
-					del(lst->content);
-				new_lst->next = lst;
 				lst = lst->next;
+				ft_lstadd_back(&new_lst, ft_lstnew(f(lst->content)));
+				if (!new_lst->next)
+					ft_lstdelone(start, del);
+				new_lst = new_lst->next;
 			}
+			return (start);
 		}
-		return (new_lst);
 	}
-	else
-		return (NULL);
+	return (NULL);
 }
